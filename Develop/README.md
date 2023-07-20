1701570d57a8b4699e0a31680a8c46b47880451b
alondra zurita <alondrazurita2@gmail.com>
2023-07-19 23:02:10 -0500

    fixed bugs

---
+++ Develop/README.md
@@ -0,0 +1,14 @@
+# Password Generator Starter Code
+## I had to help a an employee generate a strong password using the criteria I had given them. I had to make sure the password was more than 8 characters no more than 12, also that it had uppercase, lowercase and special chara ters. 
+
+## Insallation
+
+## Usage
+
+## Credits
+I used a couple different pages to get help, one helped me get started, another gave me the coding to generating a password and the last one was a tutorial on how to make it work. I was struggling to get my coding to work and the video really helped guide me.
+https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+
+https://www.youtube.com/watch?v=x4HUaiazDes&t=385s
+
+https://www.youtube.com/watch?v=v2jfGo7ztm8
--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,69 @@
 // Assignment code here
 
+// Prompt the user for the password criteria
+//    a. password length 8<128
+//    b. Lowercase, uppercase, numbers, special characters
+var password=document.getElementById("password");
+var lowerCaseArr= "abcdefghijklmnopqrstuvwxyz";
+var uppercaseArr="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharArr= "!@#$%^&*()_+";
+var numberarr="1234567890";
+var passwordLength= "12";
+var password= "";
+var choiceArr=[];
 
-// Get references to the #generate element
+// Display password to the page
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  console.log("Hey! You clicked the button!")
+  return "Generated password will go here!"
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    var correctPrompts = getPrompt();
+    var passwordText = document.querySelector("#password");
+    
+    if (correctPrompts) {
+      var newPassword = generatePassword();
+      passwordText.value = newPassword;
+    } else{
+      passwordText.value = "";
+    }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Get references to the #generate element
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * choiceArr.length);
+      password = password + choiceArr[randomIndex];
+  }
+  return password;
+}
+
+function getPrompt() {
+  choiceArr =[];
+
+  characterLength = parseInt(prompt("Password needs to be between 8-12 characters"));
+  
+  if (isNaN(passwordLength) || characterLength < 8 || characterLength > 12) {
+      alert(" Character length has to be a number between 8-12 digits. Please try again");
+      return false;
+  }
+  if (confirm("Did you want to add lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (confirm("Did you want to use uppercase in your password?")) {
+    choiceArr = choiceArr.concat(uppercaseArr);
+  }
+  if (confirm("Did you want to use special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+  if (confirm("Did you want numbers in your password")) {
+    choiceArr = choiceArr.concat(numberarr);
+  }
+  return true;
+}
